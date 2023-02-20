0053648fc47806641f9c2cabbafec797d020144d
Legion <103328140+LegionCEO@users.noreply.github.com>
2023-02-20 18:11:50 +0100

    1. Create User method 2. Create User/Dictionary/Word files 3. Added "create_user" route

--- BackEnd/routes/routers.js
+++ BackEnd/routes/routers.js
@@ -1,5 +1,9 @@
 import {Router} from "express";
+import {createUser} from "../controllers/createUser.js";
 
 const router = Router()
+
+// Create User
+router.post("/create_user", createUser)
 
 export default router
