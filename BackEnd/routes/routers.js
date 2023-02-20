96330eff7e3bfdfc7cdf6bcd7362e980de397170
Legion <103328140+LegionCEO@users.noreply.github.com>
2023-02-20 22:03:28 +0100

    1. Rename Dictionary method 2. Edit Word method

--- BackEnd/routes/routers.js
+++ BackEnd/routes/routers.js
@@ -2,6 +2,8 @@ import {Router} from "express";
 import {createUser} from "../controllers/createUser.js";
 import {createDictionary, giveDictionaryToUser} from "../controllers/createDictionary.js";
 import {createWord, giveWord} from "../controllers/createWord.js";
+import {renameDictionary} from "../controllers/renameDictionary.js";
+import {changeWord} from "../controllers/changeWord.js";
 
 const router = Router()
 
@@ -12,8 +14,14 @@ router.post("/create_user", createUser)
 router.post("/create_dic", createDictionary)
 router.get("/give_dic/:userId/:dicId", giveDictionaryToUser)
 
+// Rename Dictionary for user
+router.patch("/rename_dic/:dicId", renameDictionary)
+
 // Create Word for Dictionary
 router.post("/create_word", createWord)
 router.get("/give_word/:dicId/:wordId", giveWord)
+
+// Edit Word
+router.patch("/edit_word/:wordId", changeWord)
 
 export default router
